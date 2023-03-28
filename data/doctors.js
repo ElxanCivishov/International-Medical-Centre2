ecb5a98cdd4d93afd696b9d5e3d78644cdb676e6
Elxan Civishov <elxan.civishov2000@gmail.com>
2023-03-28 15:52:34 +0400

    update project pages

---
+++ data/doctors.js
@@ -0,0 +1,530 @@
+export const doctors = [
+  {
+    id: 1,
+    name: "İLKANƏ",
+    imgUrl: "assets/img/doctors/doc-1.jpg",
+    surname: "MƏHYƏDDİNOVA",
+    position: "Həkim-laborant",
+    degree: "Dərəcəsiz",
+    graduat: [
+      " Təfəkkür Universiteti",
+      " Tibbi biologiya İnternatura - Laboratoriya işi",
+      " Sumqayıt Şəhər Uşaq Xəstəxanası",
+    ],
+
+    work_experience: [
+      {
+        place: "İstanbul Hospital",
+        position: "Həkim laborant",
+        department: "",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim laborant",
+        department: "",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "SAMİRƏ ",
+    surname: "ƏLİYEVA",
+    imgUrl: "assets/img/doctors/doc-2.jpg",
+    position: "Həkim-anestezioloq-reanimatoloq",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+
+    work_experience: [
+      {
+        place: "Mərkəzi Neftçilər Xəstəxanası",
+        position: "Həkim-anestezioloq-reanimatoloq",
+        department:
+          "Anesteziologiya-reanimatologiya və intensiv terapiya şöbəsi",
+      },
+      {
+        place: "Mərkəzi Klinika",
+        position: "Həkim-anestezioloq-reanimatoloq",
+        department: "Cərrahiyyə şöbəsi",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim-anestezioloq-reanimatoloq",
+        department: "Cərrahiyyə və Mama Gionekologiya şöbəsi",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "CƏLALƏ",
+    surname: "ƏSGƏRLİ",
+    imgUrl: "assets/img/doctors/doc-3.jpg",
+    position: "Cərrah Mama Ginekoloq",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+
+    work_experience: [
+      {
+        place:
+          "Azərbaycan Tibb Universitetinin Tədris Cərrahiyyə Klinikası Ağcabədi Rayon Mərkəzi Xəstəxanası publik hüquqi şəxsi",
+        position: "Mamalıq-ginekologiya şöbəsi",
+        department: "Həkim mama-ginekoloq",
+      },
+      {
+        place: "'MEDİLUKS-1 MMC' (MEDİLUKS TİBB MƏRKƏZİ)",
+        position: "Ginekologiya şöbəsi",
+        department: "Həkim mama-ginekoloq",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Konsultativ poliklinika şöbəsi",
+        department: "Həkim mama-ginekoloq",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "İLQAR",
+    surname: "AĞAMƏLİYEV",
+    imgUrl: "assets/img/doctors/doc-4.jpg",
+    position: "Həkim-nevropatoloq",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+
+    work_experience: [
+      {
+        place: "Respublika Klinik Xəstəxanası",
+        position: "Həkim-nevropatoloq",
+        department: "",
+      },
+      {
+        place: "Milli Təhlukəsizlik Nazirliyi",
+        position: "Həkim-nevropatoloq",
+        department: "",
+      },
+      {
+        place: "Sərhəd Qoşunları ",
+        position: "Həkim-nevropatoloq",
+        department: "",
+      },
+      {
+        place: "Ankara Medical",
+        position: "Həkim-nevropatoloq",
+        department: "",
+      },
+      {
+        place: "Atlas Medical",
+        position: "Həkim-nevropatoloq",
+        department: "",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim-nevropatoloq",
+        department: "",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "ƏHMƏD",
+    surname: "ƏHMƏDOV",
+    imgUrl: "assets/img/doctors/doc-5.jpg",
+    position: "Həkim-kardioloq",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+
+    work_experience: [
+      {
+        place: "'MEDİLUKS-1' MMC (MEDİLUKS TİBB MƏRKƏZİ)",
+        position: "Həkim-kardioloq",
+        department: "Stasionar",
+      },
+      {
+        place: "MODERN HOSPİTAL",
+        position: "Həkim-kardioloq",
+        department: "Kardiologiya şöbəsi",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim-kardioloq",
+        department: "Konsultativ poliklinika şöbəsi",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "TƏRLAN",
+    surname: "HƏSƏNOVA",
+    imgUrl: "assets/img/doctors/doc-6.jpg",
+    position: "Cərrah Mama Ginekoloq",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+
+    work_experience: [
+      {
+        place:
+          "'Şamama Ələsgərova adına 5 nömrəli Kliniki Doğum Evi' publik hüquqi şəxsi",
+        position: "Həkim mama-ginekoloq",
+        department: "2-ci Doğum şöbəsi",
+      },
+      {
+        place:
+          "'Şamama Ələsgərova adına 5 nömrəli Kliniki Doğum Evi' publik hüquqi şəxsi",
+        position: "Şöbə müdiri (həkim-mama-ginekoloq)",
+        department: "2-ci Doğum şöbəsi",
+      },
+      {
+        place:
+          "'Şamama Ələsgərova adına 5 nömrəli Kliniki Doğum Evi' publik hüquqi şəxsi",
+        position: "Həkim-mama-ginekoloq",
+        department: "Doğum şöbəsi",
+      },
+      {
+        place: "'CASPİAN İNTERNATİONAL HOSPİTAL' MMC",
+        position: "Həkim mama-ginekoloq",
+        department: "Doğum şöbəsi",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim mama-ginekoloq",
+        department: "Konsultativ poliklinika şöbəsi",
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "NURANƏ",
+    surname: "MƏLİKZADƏ",
+    imgUrl: "assets/img/doctors/doc-7.jpg",
+    position: "Şüa diaqnostikası üzrə həkim",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+    work_experience: [
+      {
+        place:
+          "Ə.Əliyev adına Azərbaycan Dövlət Həkimləri Təkmilləşdirmə İnstitutu",
+        position: "Həkim-rezident",
+        department: "Rezidentura şöbəsi",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Şüa diaqnostikası üzrə həkim",
+        department: "Konsultativ poliklinika şöbəsi",
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "LEYLA",
+    surname: "QULİYEVA",
+    imgUrl: "assets/img/doctors/doc-8.jpg",
+    position: "Həkim-pediatr",
+    degree: "UZMAN",
+    graduat: [
+      " Azərbaycan Tibb Universiteti",
+      " İstanbul Universiteti",
+      " İstanbul  Tibb Fakültəsi (Çapa)",
+      " Uşaq sağlamlığı və Xəstəlikləri bölümü",
+    ],
+    work_experience: [
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim-pediatr",
+        department: "Neonatologiya şöbəsi",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Pediatriya bölümünün rəhbəri",
+        department: "Neonatologiya şöbəsi",
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: "YAYLAQ",
+    surname: "ŞAHVERDİYEVA",
+    imgUrl: "assets/img/doctors/doc-9.jpg",
+    position: "Cərrah Mama Ginekoloq",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Beynəlxalq Universiteti"],
+    work_experience: [
+      {
+        place: "Diaqnoz Tibb Mərkəzi MMC",
+        position: "Həkim mama-ginekoloq",
+        department: "Ginekologiya şöbəsi",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim mama-ginekoloq",
+        department: "Konsultativ poliklinika şöbəsi",
+      },
+    ],
+  },
+  {
+    id: 10,
+    name: "ƏMİRASLAN",
+    surname: "ƏMİRASLANOV",
+    imgUrl: "assets/img/doctors/doc-10.jpg",
+    position: "Estetik cərrah",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Beynəlxalq Universiteti"],
+    work_experience: [
+      {
+        place: "CASPİAN İNTERNATİONAL",
+        position: "Estetik cərrah",
+        department: "Ginekologiya şöbəsi",
+      },
+      {
+        place: "BADAM MEDİCAL",
+        position: "Estetik cərrah",
+        department: "",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Estetik cərrah",
+        department: "",
+      },
+    ],
+  },
+  {
+    id: 11,
+    name: "AFƏT",
+    surname: "TAĞIYEVA",
+    imgUrl: "assets/img/doctors/doc-11.jpg",
+    position: "Cərrah Mama Ginekoloq",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Beynəlxalq Universiteti"],
+    work_experience: [
+      {
+        place: "1 Saylı Doğum evi",
+        position: "Cərrah Mama Ginekoloq",
+        department: "Ginekologiya şöbəsi",
+      },
+      {
+        place: "German Hospital",
+        position: "Cərrah Mama Ginekoloq",
+        department: "",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Cərrah Mama Ginekoloq",
+        department: "",
+      },
+    ],
+  },
+  {
+    id: 12,
+    name: "ELVİRA",
+    surname: "MİRZƏYEVA",
+    imgUrl: "assets/img/doctors/doc-12.jpg",
+    position: "Həkim terapevt, Ailə həkimi , ANTİ-AGE üzrə həkim",
+    degree: "Dərəcəsiz",
+    graduat: ["Nəriman Nərimanov adına Azərbaycan Dövlət Tibb İnstitutu"],
+    work_experience: [
+      {
+        place: "MONO klinikası",
+        position: "Həkim terapevt",
+        department: "",
+      },
+      {
+        place: "ABU klinikası",
+        position: "Terapiya şöbəsinin müdiri",
+        department: "",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim terapevt",
+        department: "Konsultativ poliklinika şöbəsi",
+      },
+    ],
+  },
+  {
+    id: 13,
+    name: "RƏŞAD",
+    surname: "NURİYEV",
+    imgUrl: "assets/img/doctors/doc-2.jpg",
+    position: "Həkim-anestezioloq-reanimatoloq",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+    work_experience: [
+      {
+        place: "LEYLA MEDICAL CENTER",
+        position: "Şöbə müdiri",
+        department:
+          "Anesteziologiya-reanimatologiya və intensiv terapiya şöbəsi",
+      },
+      {
+        place: "Mərkəzi Neftçilər Xəstəxanası",
+        position: "Həkim-anestezioloq-reanimatoloq",
+        department:
+          "Anesteziologiya-reanimatologiya və intensiv terapiya şöbəsi",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim-anestezioloq-reanimatoloq",
+        department: "Cərrahiyyə şöbəsi",
+      },
+    ],
+  },
+  {
+    id: 14,
+    name: "AZƏR",
+    surname: "QAFAROV",
+    imgUrl: "assets/img/doctors/doc-14.jpg",
+    position: "Həkim travmatoloq - ortoped",
+    degree: "Tibb ürzə Fəlsəfə doktoru",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+    work_experience: [
+      {
+        place:
+          "Milli Təhlükəsizlik Nazirliyi və Dövlət Sərhəd Xidməti Hərbi Hospitalı",
+        position: "Polkovnik Leytenant",
+        department: "Travmatologiya şöbəsinin rəisi",
+      },
+      {
+        place: "ELVA özəl tibb müəssisəsi",
+        position: "Həkim travmatoloq - ortoped",
+        department: "",
+      },
+      {
+        place: "Real Tibb Mərkəzi",
+        position: "Həkim travmatoloq - ortoped",
+        department: "",
+      },
+      {
+        place: "Respublika İdman Tibb Reabilitasiya və Diaqnostika institutu",
+        position: "Həkim travmatoloq - ortoped",
+        department: "",
+      },
+      {
+        place:
+          "Səudiyyə Ərəbistan krallığı, Hafer Al Batin Mərkəzi Xəstəxanası",
+        position: "Həkim travmatoloq - ortoped",
+        department: "",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim travmatoloq - ortoped",
+        department: "",
+      },
+    ],
+  },
+  {
+    id: 15,
+    name: "ÜLVİYYƏ",
+    surname: "QÜDRƏTOVA",
+    imgUrl: "assets/img/doctors/doc-15.jpg",
+    position: "Cərrah Mama Ginekoloq",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+    work_experience: [
+      {
+        place: "Ağcabədi Rayon Mərkəzi Xəstəxana",
+        position: "Cərrah Mama Ginekoloq",
+        department: "",
+      },
+      {
+        place: "Lazer Med",
+        position: "Cərrah Mama Ginekoloq",
+        department: "",
+      },
+      {
+        place: "Doktor Davud Tibb Mərkəzi",
+        position: "Cərrah Mama Ginekoloq",
+        department: "",
+      },
+      {
+        place: "Binəqədi Rayon 9 saylı Qadın Məsləhətxanası",
+        position: "Cərrah Mama Ginekoloq",
+        department: "",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Cərrah Mama Ginekoloq",
+        department: "",
+      },
+    ],
+  },
+  {
+    id: 16,
+    name: "ŞƏFƏQ",
+    surname: "ƏKBƏROVA",
+    imgUrl: "assets/img/doctors/doc-16.jpg",
+    position: "Cərrah Mama Ginekoloq",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+    work_experience: [
+      {
+        place: "Şəhər Onkoloji Dispanser",
+        position: "Həkim Ginekoloq",
+        department: "Ginekologiya Şöbəsi",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim Ginekoloq",
+        department: "",
+      },
+    ],
+  },
+  {
+    id: 17,
+    name: "AYNURƏ",
+    surname: "ABDULLAYEVA",
+    imgUrl: "assets/img/doctors/doc-17.jpg",
+    position: "Şüa diaqnostikası üzrə həkim",
+    degree: "Dərəcəsiz",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+    work_experience: [
+      {
+        place: "Sağlam mühit MMC",
+        position: "Şüa diaqnostikası üzrə həkim",
+        department: "Şüa diaqnostikası şöbəsi",
+      },
+      {
+        place: "Spero Hospital MMC",
+        position: "Şüa diaqnostikası üzrə həkim",
+        department: "Şüa diaqnostikası şöbəsi",
+      },
+      {
+        place: "STİMUL.S HOSPİTAL",
+        position: "Şüa diaqnostikası üzrə həkim",
+        department: "Poliklinika şöbəsi",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Şüa diaqnostikası üzrə həkim",
+        department: "Şüa diaqnostikası şöbəsi",
+      },
+    ],
+  },
+  {
+    id: 18,
+    name: "SEVİNC",
+    surname: "QARAYEVA",
+    imgUrl: "assets/img/doctors/doc-18.jpg",
+    position: "Həkim mama-ginekoloq",
+    degree: "Tibb elmləri namizədi",
+    graduat: ["Azərbaycan Tibb Universiteti"],
+    work_experience: [
+      {
+        place: "Bakı Sağlamlıq Mərkəzi",
+        position: "Həkim mama-ginekoloq",
+        department: "Süni mayalandırma şöbəsi",
+      },
+      {
+        place: "Azərbaycan Tibb Universiteti",
+        position: "Şöbə müdiri",
+        department: "Eksperimental cərrahi şöbə",
+      },
+      {
+        place: "'MEDİLUKS-1' MMC (MEDİLUKS TİBB MƏRKƏZİ)",
+        position: "Həkim mama-ginekoloq",
+        department: "Ginekologiya şöbəsi",
+      },
+      {
+        place: "'İNTERNATİONAL MEDİCAL CENTRE' MMC",
+        position: "Həkim mama-ginekoloq",
+        department: "Doğum şöbəsi",
+      },
+    ],
+  },
+];
